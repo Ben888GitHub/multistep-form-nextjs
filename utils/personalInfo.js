b04414dc87d191af1391c86e33025112769cd629
Benedict <benedictryan80@gmail.com>
2023-04-24 15:46:20 +0700

    destructure errors value

--- utils/personalInfo.js
+++ utils/personalInfo.js
@@ -1,38 +1,42 @@
 export const basicInfo = (errors) => {
+	const { firstName, lastName, email } = errors;
+
 	return [
 		{
 			label: 'First name',
-			error: errors.firstName,
+			error: firstName,
 			value: 'firstName'
 		},
 		{
 			label: 'Last name',
-			error: errors.lastName,
+			error: lastName,
 			value: 'lastName'
 		},
 		{
 			label: 'Email address',
-			error: errors.email,
+			error: email,
 			value: 'email'
 		}
 	];
 };
 
 export const locationInfo = (errors) => {
+	const { city, region, postalCode } = errors;
+
 	return [
 		{
 			label: 'City',
-			error: errors.city,
+			error: city,
 			value: 'city'
 		},
 		{
 			label: 'State / Province',
-			error: errors.region,
+			error: region,
 			value: 'region'
 		},
 		{
 			label: 'ZIP / Postal code',
-			error: errors.postalCode,
+			error: postalCode,
 			value: 'postalCode'
 		}
 	];
